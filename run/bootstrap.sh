fb2247d830a04868dcf28a53b16368b2cfeede15
Kieran Potts <hello@kieranpotts.com>
2024-08-21 19:13:41 +0100

    refactor: distinguish dev utilities from runtime environments

--- run/bootstrap.sh
+++ run/bootstrap.sh
@@ -18,7 +18,15 @@ source "${inc}/sys/update.sh"
 source "${inc}/sys/upgrade.sh"
 
 # Install dev tools.
-source "${inc}/get/tmux.sh"
+source "${inc}/util/delta.sh"
+source "${inc}/util/git.sh"
+source "${inc}/util/git-extras.sh"
+source "${inc}/util/git-lfs.sh"
+source "${inc}/util/lazygit.sh"
+source "${inc}/util/neovim.sh"
+source "${inc}/util/oh-my-posh.sh"
+source "${inc}/util/tmux.sh"
+
 
 # Finalise.
 source "${inc}/sys/teardown.sh"
