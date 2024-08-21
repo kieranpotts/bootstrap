0f3691176e708ffeaaed7f055f7d90fdce0908c1
Kieran Potts <hello@kieranpotts.com>
2024-08-21 20:18:55 +0100

    maintenance: edit inline comments

--- run/_/sys/update.sh
+++ run/_/sys/update.sh
@@ -12,7 +12,12 @@ startNewTask "Updating the system"
 
 sudo timedatectl set-timezone UTC
 
+# Clean up any packages that were installed to satisfy dependencies,
+# but which are no longer needed.
 sudo apt-get -y autoremove
+
+# Purge unused packages and clean up the package cache.
 sudo apt-get -y --purge remove && sudo apt-get autoclean
 
+# Update the package list.
 sudo apt-get update
