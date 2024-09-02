4897df498bbc15e3a0582f71cbedc68330e81a29
Kieran Potts <hello@kieranpotts.com>
2024-09-02 12:12:50 +0100

    maintenance: add comments on git-extras install

--- run/_/dev/git-extras.sh
+++ run/_/dev/git-extras.sh
@@ -2,7 +2,9 @@
 
 # ==============================================================================
 # Install `git-extras`, a popular package that extends git with additional
-# commands.
+# commands. This will add a whole bunch of `git-*` files to the `/usr/bin`
+# directory, which Git will automatically make available as `git` subcommands
+# (aliases).
 #
 # https://github.com/tj/git-extras
 # ==============================================================================
