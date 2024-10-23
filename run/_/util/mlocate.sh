491ee53bfca7bfbbff6321a614556374389f6771
Kieran Potts <hello@kieranpotts.com>
2024-10-23 09:35:29 +0100

    maintenance: add TODO for mlocate package

--- run/_/util/mlocate.sh
+++ run/_/util/mlocate.sh
@@ -10,5 +10,6 @@
 startNewTask "Install mlocate"
 
 # This is a very slow operation, the first time it is run, as a database
-# is compiled of all files on the system.
+# is compiled of all files on the system. TODO: Investigate if this can be
+# done on the background or otherwise deferred to a susequent manual step.
 sudo apt install mlocate
