fe98a4c906a068aabf0cf2b90e26fc98bc23e8fc
Kieran Potts <kieranpotts@users.noreply.github.com>
2025-11-03 13:29:34 +0000

    feature: add claude code

---
+++ run/_/dev/claude.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ==============================================================================
+# Installs Claude Code.
+#
+# Requires Node.js and NPM.
+#
+# https://docs.anthropic.com/en/docs/claude-code/overview
+# ==============================================================================
+
+startNewTask "Installing Claude Code via NPM"
+
+# Requires Node.js v18 or newer.
+npm install -g @anthropic-ai/claude-code
