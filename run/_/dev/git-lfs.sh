eb91f58e696a072d7c96a2a55d1797b88468049c
Kieran Potts <kieranpotts@users.noreply.github.com>
2026-03-20 19:13:35 +0000

    dev: review git lfs installation script

--- run/_/dev/git-lfs.sh
+++ run/_/dev/git-lfs.sh
@@ -8,11 +8,8 @@
 
 startNewTask "Install Git LFS"
 
-(. /etc/lsb-release &&
-  curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh |
-  superdo env os=ubuntu dist="${DISTRIB_CODENAME}" bash)
-
-superdo apt-get install git-lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | superdo bash
+superdo apt-get install git-lfs=3.7.1
 
 # Enable LFS in Git.
 git lfs install
