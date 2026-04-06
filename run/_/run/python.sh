9a66ca6d3f1e2a58eef750471cc873e12b9ec29f
Kieran Potts <kieranpotts@users.noreply.github.com>
2026-04-06 06:31:41 +0100

    dev: update logic for adding ~/.local/bin to path

--- run/_/run/python.sh
+++ run/_/run/python.sh
@@ -4,6 +4,9 @@
 # Install Python 3 and pip, and related tools.
 # ==============================================================================
 
+# Single quotes are used in this file to prevent variable expansion.
+# shellcheck disable=SC2016
+
 startNewTask "Install Python, pip, pipenv, etc."
 
 superdo apt install -y python3 python3-pip ipython3
@@ -15,19 +18,19 @@ pip install --user pipenv
 curl -sSL https://install.python-poetry.org | python3 -
 
 # Add ~/.local/bin to PATH, required for the virtualenv script which is
-# installed here, see:
+# installed here. See:
 # https://pipenv.pypa.io/en/latest/installation.html#installing-pipenv
-if [ -f "$HOME/local.bashrc" ]; then
-  # (Single quotes are used to prevent variable expansion.)
-  # shellcheck disable=SC2016
-  if ! grep -q 'export PATH="$HOME/.local/bin:$PATH"' "$HOME/local.bashrc"; then
-    echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/local.bashrc"
-    . "$HOME/local.bashrc"
-  fi
+#
+# I have this in my global.bashrc dotfiles by default, so look for it there
+# first, before adding to local.bashrc. Fallback to  ~/.bashrc. See
+#
+if [ -f "$HOME/.bashrc" ] && grep -q 'export PATH="$HOME/.local/bin:$PATH"' "$HOME/.bashrc"; then
+  : # Already configured in global.bashrc dotfiles, nothing to do.
+elif [ -f "$HOME/local.bashrc" ] && ! grep -q 'export PATH="$HOME/.local/bin:$PATH"' "$HOME/local.bashrc"; then
+  echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/local.bashrc"
+  . "$HOME/local.bashrc"
 else
   touch "$HOME/.bashrc"
-  # (Single quotes are used to prevent variable expansion.)
-  # shellcheck disable=SC2016
   if ! grep -q 'export PATH="$HOME/.local/bin:$PATH"' "$HOME/.bashrc"; then
     echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
     . "$HOME/.bashrc"
