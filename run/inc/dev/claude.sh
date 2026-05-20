269e5722d92990f1fd73227a38ad2d76132b490d
Kieran Potts <7712355+kieranpotts@users.noreply.github.com>
2026-05-20 18:01:53 +0100

    fix: access to npm in non-interactive subshells

--- run/inc/dev/claude.sh
+++ run/inc/dev/claude.sh
@@ -10,6 +10,12 @@
 
 print_step "Installing Claude Code."
 
+# Install steps are run in non-interactive subshells.
+# Need to re-source NVM, so npm is available in the subshell.
+if [[ -s "${HOME}/.nvm/nvm.sh" ]]; then
+  . "${HOME}/.nvm/nvm.sh"
+fi
+
 print_info "Installing/updating Claude Code globally via NPM."
 npm install -g @anthropic-ai/claude-code
 claude --version
--- run/inc/dev/copilot.sh
+++ run/inc/dev/copilot.sh
@@ -12,6 +12,12 @@
 
 print_step "Install Copilot CLI."
 
+# Install steps are run in non-interactive subshells.
+# Need to re-source NVM, so npm is available in the subshell.
+if [[ -s "${HOME}/.nvm/nvm.sh" ]]; then
+  . "${HOME}/.nvm/nvm.sh"
+fi
+
 print_info "Installing/updating GitHub Copilot CLI globally via NPM."
 npm install -g @github/copilot
 copilot --version
