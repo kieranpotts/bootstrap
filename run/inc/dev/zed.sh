fdf9390d274f768fcd24f9bb68e658cf9e64dc7b
Kieran Potts <7712355+kieranpotts@users.noreply.github.com>
2026-05-20 12:03:13 +0100

    feature: install zed editor

---
+++ run/inc/dev/zed.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#
+# Install Zed editor.
+#
+# https://zed.dev/docs/installation
+#
+
+is_gui_enabled || return 0
+
+print_step "Installing Zed."
+
+print_info "Installing/updating Zed via official install.sh script."
+curl -f https://zed.dev/install.sh | sh
