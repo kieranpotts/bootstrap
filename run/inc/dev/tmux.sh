a520f81d70656bfd9807023cf5585ffa9869bad9
Kieran Potts <7712355+kieranpotts@users.noreply.github.com>
2026-06-23 07:26:28 +0100

    feature: install tmux plugin manager (tpm)

--- run/inc/dev/tmux.sh
+++ run/inc/dev/tmux.sh
@@ -11,3 +11,16 @@ print_step "Installing tmux."
 print_info "Installing/updating tmux via APT."
 superdo apt-get install -y tmux
 tmux -V
+
+# TPM (Tmux Plugin Manager) lets ~/.tmux.conf declare plugins (eg.
+# tmux-resurrect/tmux-continuum for session persistence across reboots)
+# without manual git management.
+# https://github.com/tmux-plugins/tpm
+
+if [[ -d "${HOME}/.tmux/plugins/tpm" ]]; then
+  print_info "TPM is already installed. Pulling latest."
+  git -C "${HOME}/.tmux/plugins/tpm" pull
+else
+  print_info "Installing TPM."
+  git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+fi
